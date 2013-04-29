6b6a6c94339386d78d362dfdb7130e7c591db730
Dmitry <dmitry@park0ur.ru>
2013-04-29 16:25:45 +0300

    Initial Commit

---
+++ PathIntro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PathIntro
+//
+//  Created by Dmitry Kondratyev on 29.04.13.
+//  Copyright (c) 2013 Dmitry Kondratyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PathIntro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PathIntro
+//
+//  Created by Dmitry Kondratyev on 29.04.13.
+//  Copyright (c) 2013 Dmitry Kondratyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
